97e45daf06a321569a9bbb20ea72392f1705f174
Mauricio Coelho <mauscoelho@gmail.com>
2016-01-12 18:17:11 -0200

    :art: bind routes in the recyclerview

--- ResearcherRoutes/app/src/main/java/com/mauscoelho/researcherroutes/ui/activities/MainActivity.java
+++ ResearcherRoutes/app/src/main/java/com/mauscoelho/researcherroutes/ui/activities/MainActivity.java
@@ -1,40 +1,84 @@
 package com.mauscoelho.researcherroutes.ui.activities;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.mauscoelho.researcherroutes.R;
 import com.mauscoelho.researcherroutes.network.interfaces.IAction;
 import com.mauscoelho.researcherroutes.network.models.Route;
 import com.mauscoelho.researcherroutes.network.services.RouteService;
+import com.mauscoelho.researcherroutes.ui.adapters.RouteAdapter;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    RouteService _routeService = new RouteService();
+    private RouteService _routeService = new RouteService();
+    private ProgressBar loader;
+    private RecyclerView rv_route;
+    private LinearLayoutManager _linearLayoutManager;
+    private Activity activity = this;
+    private RouteAdapter routeAdapter = null;
+    private Toolbar toolbar;
+    private EditText toolbar_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FindById();
+        SetToolbar();
+    }
 
-        searchRoute("lauro linhares");
+    private void FindById() {
+        rv_route = (RecyclerView) findViewById(R.id.rv_route);
+        loader = (ProgressBar) findViewById(R.id.loader);
+        _linearLayoutManager = new LinearLayoutManager(this);
+        _linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        rv_route.setLayoutManager(_linearLayoutManager);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+    }
+
+    private void SetToolbar() {
+        View view = toolbar.getRootView();
+        ImageView toolbar_search =  (ImageView)view.findViewById(R.id.toolbar_search);
+        toolbar_text = (EditText)view.findViewById(R.id.toolbar_text);
+        toolbar_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchRoute(toolbar_text.getText().toString());
+            }
+        });
+
     }
 
     private void searchRoute(String stopName) {
-        _routeService.findRoutesByStopName(new IAction<List<Route>>() {
-            @Override
-            public void OnCompleted(List<Route> routes) {
+        if(!stopName.isEmpty()) {
+            loader.setVisibility(View.VISIBLE);
+            _routeService.findRoutesByStopName(new IAction<List<Route>>() {
+                @Override
+                public void OnCompleted(List<Route> routes) {
+                    loader.setVisibility(View.GONE);
+                    routeAdapter = new RouteAdapter(routes, activity);
+                    rv_route.setAdapter(routeAdapter);
+                }
 
-            }
-
-            @Override
-            public void OnError(List<Route> routes) {
-
-            }
-        }, stopName);
+                @Override
+                public void OnError(List<Route> routes) {
+                    loader.setVisibility(View.GONE);
+                }
+            }, stopName);
+        }
     }
 }
--- ResearcherRoutes/app/src/main/java/com/mauscoelho/researcherroutes/ui/adapters/RouteAdapter.java
+++ ResearcherRoutes/app/src/main/java/com/mauscoelho/researcherroutes/ui/adapters/RouteAdapter.java
@@ -3,9 +3,12 @@ package com.mauscoelho.researcherroutes.ui.adapters;
 
 import android.app.Activity;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.mauscoelho.researcherroutes.R;
 import com.mauscoelho.researcherroutes.network.models.Route;
 
 import java.util.List;
@@ -45,12 +48,12 @@ public class RouteAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
 
         if (viewType == 0) {
-            /*View view = LayoutInflater.
+            View view = LayoutInflater.
                     from(viewGroup.getContext()).
-                    inflate(R.layout.card_match, viewGroup, false);
+                    inflate(R.layout.card_route, viewGroup, false);
 
 
-            return new RouteViewHolder(view);*/
+            return new RouteViewHolder(view);
         }
 
         return null;
@@ -59,15 +62,18 @@ public class RouteAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
 
     private void bindMatchItem(RouteViewHolder viewHolder, final Route route) {
         RouteViewHolder routeViewHolder = viewHolder;
+        viewHolder.route_shortName.setText(route.shortName);
 
     }
 
 
     public static class RouteViewHolder extends RecyclerView.ViewHolder {
-
+        protected TextView route_shortName;
 
         public RouteViewHolder(View v) {
             super(v);
+
+            route_shortName = (TextView)v.findViewById(R.id.route_shortName);
 
         }
     }
